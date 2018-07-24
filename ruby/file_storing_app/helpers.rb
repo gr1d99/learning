a45965c35f3cd74d9093f0991b3a7091cdaa8e7a
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-07-24 12:46:26 +0300

    add listing of stored files functionality (#5)

--- ruby/file_storing_app/helpers.rb
+++ ruby/file_storing_app/helpers.rb
@@ -2,10 +2,14 @@ require_relative './constants'
 
 module FileStorageHelpers
   def self.verify_file_in_store(filename)
-    File.exist?("#{STORAGE_DIRECTORY}/#{filename}")
+    File.exist?("#{STORAGE_DIRECTORY_NAME}/#{filename}")
   end
 
   def self.storage_path
-    File.absolute_path(STORAGE_DIRECTORY.to_s)
+    File.absolute_path(STORAGE_DIRECTORY_NAME.to_s)
+  end
+
+  def self.clear_screen
+    Gem.win_platform? ? (system 'cls') : (system 'clear')
   end
 end
